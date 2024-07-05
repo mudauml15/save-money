b20430e9e336be5abeb006946f389086db9c70a7
Nellark <nelakulati@gmail.com>
2024-07-05 08:13:03 +0200

    Added a function to update input

--- main.js
+++ main.js
@@ -13,9 +13,32 @@ function addTransaction() {
     return;
 
 }
-let transaction = { detail, amount, type };
-transactions.push(transaction);
-updateInput();
+if (type === 'expense' && incomeTotal === 0) {
+  alert('Cannot add expense when income is not set.');
+  return;
+}
+
+if (type === 'expense' && amount > incomeTotal - expenseTotal) {
+  let remainingIncome = incomeTotal - expenseTotal;
+  alert(`Expenses cannot exceed income. You have R${remainingIncome.toFixed(2)} left.`);
+  return;
+}
+
+let existingTransaction = transactions.find(tran => tran.detail === detail && tran.type === type);
+
+if (existingTransaction) {
+  existingTransaction.amount = (parseFloat(existingTransaction.amount) + amount).toFixed(2);
+  updateInput(); // Update Input after modifying existing transaction
+} else {
+  let transaction = {
+    detail: detail,
+    amount: amount.toFixed(2),
+    type: type
+  };
+
+  transactions.push(transaction);
+  updateInput(); // Update Input after adding new transaction
+}
 
 document.getElementById('txtDetail').value = '';
 document.getElementById('txtAmount').value = 0;
@@ -23,4 +46,5 @@ document.getElementById('txtAmount').value = 0;
 localStorage.setItem('transactions', JSON.stringify(transactions));
 
 }
+
 
